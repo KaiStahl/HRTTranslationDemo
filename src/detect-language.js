1443109d6d699280c48c19c6cf3a6e5883af868c
Kai Stahl <kai.stahl@student.reutlingen-university.de>
2019-11-21 13:20:09 +0100

    identify language started again

--- src/detect-language.js
+++ src/detect-language.js
@@ -8,13 +8,13 @@ const { IamAuthenticator } = require('ibm-watson/auth');
  * @param {*} defaultLanguage 
  */
 function getTheErrorResponse(errorMessage, defaultLanguage) {
-  return {
-    statusCode: 200,
-    body: {
-      language: defaultLanguage || 'en',
-      errorMessage: errorMessage
-    }
-  };
+    return {
+        statusCode: 200,
+        body: {
+            language: defaultLanguage || 'en',
+            errorMessage: errorMessage
+        }
+    };
 }
 
 /**
@@ -28,59 +28,76 @@ function getTheErrorResponse(errorMessage, defaultLanguage) {
   */
 function main(params) {
 
-  /*
-   * The default language to choose in case of an error
-   */
-  const defaultLanguage = 'en';
+    /*
+     * The default language to choose in case of an error
+     */
+    const defaultLanguage = 'en';
 
-  return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve, reject) {
 
-    try {
-      
-      // *******TODO**********
-      // - Call the language identification API of the translation service
-      // see: https://cloud.ibm.com/apidocs/language-translator?code=node#identify-language
-      // - if successful, resolve exactly like shown below with the
-      // language that is most probable the best one in the "language" property
-      // and the confidence it got detected in the "confidence" property
+        try {
+            const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+            const { IamAuthenticator } = require('ibm-watson/auth');
 
-      // in case of errors during the call resolve with an error message according to the pattern 
-      // found in the catch clause below
-		identify(params, [callback()])
-	  
-		const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-		const { IamAuthenticator } = require('ibm-watson/auth');
+            const languageTranslator = new LanguageTranslatorV3({
+                version: '2018-05-01',
+                authenticator: new IamAuthenticator({
+                    apikey: 'n4bYLHtVR297cb3cgm-lphwjoHR6_Pmz9DpV6CxYPWoU',
+                }),
+                url: 'https://gateway-fra.watsonplatform.net/language-translator/api',
+            });
 
-		const languageTranslator = new LanguageTranslatorV3({
-			version: '2018-05-01',
-			authenticator: new IamAuthenticator({
-			apikey: '{apikey}',
-		}),
-		url: '{url}',
-		});
+            const identifyParams = {
+                text: 'Language translator translates text from one language to another'
+            };
 
-		const identifyParams = {
-			text: 'Language translator translates text from one language to another'
-		};
+            languageTranslator.identify(identifyParams)
+                .then(identifiedLanguages => {
+                    console.log(JSON.stringify(identifiedLanguages));
+                    resolve({
+                        statusCode: 200,
+                        body: {
+                            text: params.text,
+                            language: "<Best Language>",
+                            confidence: 0.5,
+                        },
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                })
 
-		languageTranslator.identify(identifyParams).then(identifiedLanguages => {console.log(JSON.stringify(identifiedLanguages));
-			resolve({
-        statusCode: 200,
-        body: {
-          text: params.text, 
-          language: "<Best Language>",
-          confidence: 0.5,
-        },
-        headers: { 'Content-Type': 'application/json' }
-      });
-		})
-		.catch(err => {
-			catch (err) {
-			console.error('Error while initializing the AI service', err);
-			resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
-    }
-		console.log('error:', err);
-		});
-	} 
-  });
+                .catch(err => {
+                    console.log('error:', err);
+                    resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
+                    });
+
+
+            // *******TODO**********
+            // - Call the language identification API of the translation service
+            // see: https://cloud.ibm.com/apidocs/language-translator?code=node#identify-language
+            // - if successful, resolve exactly like shown below with the
+            // language that is most probable the best one in the "language" property
+            // and the confidence it got detected in the "confidence" property
+
+            // in case of errors during the call resolve with an error message according to the pattern 
+            // found in the catch clause below
+
+           /* resolve({
+                statusCode: 200,
+                body: {
+                    text: params.text,
+                    language: "<Best Language>",
+                    confidence: 0.5,
+                },
+                headers: { 'Content-Type': 'application/json' }
+            });
+            */
+
+            
+        } catch (err) {
+            console.error('Error while initializing the AI service', err);
+            resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
+        }
+
+            
+    });
 }
